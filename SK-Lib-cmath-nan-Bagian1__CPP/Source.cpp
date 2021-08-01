84a51a759d796817fdfb18684cc9e3f3d7b89c03
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-01 08:36:07 +0700

    Add project files.

---
+++ SK-Lib-cmath-nan-Bagian1__CPP/Source.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <cmath> // for sqrt()
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	float a = 2, b = -2;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl << endl;
+
+	cout << "sqrt(a) = " << sqrt(a) << endl;
+	cout << "sqrt(b) = " << sqrt(b) << endl;
+
+	_getch();
+	return 0;
+}
